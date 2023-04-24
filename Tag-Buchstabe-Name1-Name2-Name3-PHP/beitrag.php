824b5f682ef6e7a3e8e628d7c932f4146900280b
Robin <robe123@hotmail.de>
2023-04-24 18:44:03 +0200

    1

---
+++ Tag-Buchstabe-Name1-Name2-Name3-PHP/beitrag.php
@@ -0,0 +1,42 @@
+<?php include_once "php/head.php" ?>
+
+<body>
+
+    <?php include_once "php/nav.php" ?>
+
+    <main>
+
+        <h1>Eintrag</h1>
+        <h2>Titel</h2>
+        <h4>Author</h4>
+        <h4>Datum</h4>
+        <p>ORT</p>
+        <p>TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT </p>
+        <img src="images/guestbook.png" alt="Gästebuch">
+        <input type="submit" value="Bearbeiten">
+       
+
+        
+        <h3>Kommentare</h3>
+        <p>Name</p>
+        <p>Kommentar</p>
+        <input type="submit" value="Bearbeiten">
+        <p>Name</p>
+        <p>Kommentar</p>
+        <input type="submit" value="Bearbeiten">
+        <p>Name</p>
+        <p>Kommentar</p>
+        <input type="submit" value="Bearbeiten">
+        <p>Name</p>
+        <p>Kommentar</p>
+        <input type="submit" value="Bearbeiten">
+
+
+
+
+    </main>
+
+    <?php include_once "php/footer.php" ?>
+</body>
+
+</html>
