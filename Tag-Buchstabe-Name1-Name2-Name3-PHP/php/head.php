e9ca3efe828f826446ac9503262bca554868de32
Robin <robe123@hotmail.de>
2023-04-24 16:45:15 +0200

    php base

---
+++ Tag-Buchstabe-Name1-Name2-Name3-PHP/php/head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Gästebuch Beispiel">
+    <meta name="author" content="Dietrich Boles">
+    <link rel="stylesheet" href="css/main.css" />
+    <title>Gästebuch</title>
+</head>
---
+++ Tag-Buchstabe-Name1-Name2-Name3-PHP/php/nav.php
@@ -0,0 +1,11 @@
+<nav>
+    <div>
+        <a href="index.php">Homepage</a>
+    </div>
+    <div>
+        <a href="anmeldung.php">Anmelden/Abmelden</a>
+    </div>
+    <div>
+        <a href="registrierung.php">Registrieren</a>
+    </div>
+</nav>
