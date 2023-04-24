a517830bfdd305ce2a0b95a8be00decbfe259aed
Robin <robe123@hotmail.de>
2023-04-24 18:27:13 +0200

    1

--- Tag-Buchstabe-Name1-Name2-Name3-PHP/impressum.php
+++ Tag-Buchstabe-Name1-Name2-Name3-PHP/impressum.php
@@ -9,13 +9,40 @@
         <h1>Impressum</h1>
 
         <p>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-            dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-            clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-            consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-            sea takimata sanctus est Lorem ipsum dolor sit amet.
-        </p>
+
+        <h2> Herausgeber </h2>
+        Carl von Ossietzky Universität Oldenburg <br>
+        Ammerländer Heerstr. 114-118 <br>
+        26129 Oldenburg <br>
+        <br>
+        Telefon: +49 441 798-0 <br>
+        Telefax: +49 441 798-3000 <br>
+        E-Mail: internet@uol.de <br>
+        Internet: www.uol.de <br>
+        <br>
+        Die Universität Oldenburg ist eine Körperschaft des Öffentlichen Rechts. Sie wird durch den Präsidenten gesetzlich vertreten. <br>
+        <br>
+        Zuständige Aufsichtsbehörde: <br>
+        Niedersächsisches Ministerium für Wissenschaft und Kultur (MWK), Leibnizufer 9 (Postfach 261), 30002 Hannover <br>
+        <br>
+        Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz: <br>
+        DE 811184499 <br>
+        <br>
+        Inhaltlich Verantwortlicher gemäß § 18 Abs. 2 MStV: <br>
+        Volker Burggräf, Internetkoordinator der Universität Oldenburg bzw. die jeweils im Seitenfuß genannte Person bzw. Einrichtung (vertreten durch ihren derzeitigen Leiter/Leiterin) für Angebote auf dem zentralen Server der Universität. <br>
+        <br>
+        Daneben gibt es dezentrale Angebote, z. T. auf eigenen Servern, für die jeweils eigene Verantwortliche ersichtlich sein müssen. <br>
+        <br>
+        Webseiten, die mit www.<servername.>uni-oldenburg.de/~, www.member.uni-oldenburg.de/, www.staff.uni-oldenburg.de/ oder www.student.uni-oldenburg.de/ beginnen, unterliegen der Verantwortung der jeweiligen Einzelpersonen (~username bzw. vorname.nachname). Auf diese Inhalte hat die Universität keinen Einfluss, somit stellen sie keinesfalls offizielle Publikationen der Universität dar. <br>
+            <br>
+            Haftungshinweis: <br>
+            Mit Urteil vom 12. Mai 1998 hat das Landgericht Hamburg entschieden, dass man durch die Ausbringung eines Links - die Inhalte der gelinkten Seite ggfs. mit zu verantworten hat. Dies kann - so das Landgericht Hamburg - nur dadurch verhindert werden, dass man sich ausdrücklich von den Inhalten der gelinkten Seiten distanziert. Darum erklären wir hiermit: Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.<br>
+
+            Informationen zum Copyright
+            Der Inhalt dieser Webseiten ist urheberrechtlich geschützt. Alle Abbildungen und Fotos auf den Webseiten der Carl von Ossietzky Universität Oldenburg dürfen nicht ohne Genehmigung übernommen, vervielfältigt und verbreitet werden. <br>
+            <br>
+            Koordination der Anwendung: Internetkoordinator (Presse & Kommunikation) <br>
+            </p>
 
     </main>
     <?php include_once "php/footer.php" ?>
