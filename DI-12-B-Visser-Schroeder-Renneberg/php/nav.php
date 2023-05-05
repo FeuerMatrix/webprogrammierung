d88a0d71296a374a5cd7896520fe55f517d98df3
Nils Renneberg <nils.renneberg@uni-oldenburg.de>
2023-05-05 20:41:19 +0200

    Naviagtion Bar

--- DI-12-B-Visser-Schroeder-Renneberg/php/nav.php
+++ DI-12-B-Visser-Schroeder-Renneberg/php/nav.php
@@ -1,24 +1,18 @@
-<style>
-  .grid {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    background-color: #333;
-    position: fixed;
-    top: 0;
-    width: 100%;
-  }
-</style>
+<link rel="stylesheet" href="css/nav.css" >
 
-<div class="grid">
-  <div>
-  <li> <a href="index.php">Homepage</a>
-  <li> <a href="hauptseite.php">Beiträge</a>
-  <li> <a href="eintragneu.php">Neuer Beitrag</a>
-  </div>
-  <div style="display: flex; justify-content: flex-end">
-  <li> <a href="anmeldung.php">Anmelden</a>
-  <li> <a href="index.php">Abmelden</a>
-  <li> <a href="registrieren.php">Registrieren</a>
-  </div>
-</div>
+<nav>
+  <ul class="nav-mobil"> <!-- über java Skript für SimpleToggle-->
+    <li> <a id="nav-links-mobil"> Menu </a> 
+    <li> <a id="nav-rechts-mobil"> Anmelden </a>
+  </ul>
+  <ul class="nav-links open"> <!-- open Tag wird später mit javaSkript gesetzt oder entfernt, garde noch drin damit man sieht wie es später ausgeklappt aussieht -->
+    <li> <a href="index.php">Homepage</a>
+    <li> <a href="hauptseite.php">Beiträge</a>
+    <li> <a href="eintragneu.php">Neuer Beitrag</a>
+  </ul>
+  <ul class="nav-rechts open">
+    <li> <a href="anmeldung.php">Anmelden</a>
+    <li> <a href="index.php">Abmelden</a>
+    <li> <a href="registrieren.php">Registrieren</a>
+  </ul>
+</nav>
