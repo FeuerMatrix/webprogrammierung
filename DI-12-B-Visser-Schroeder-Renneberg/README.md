d28b2be4b73b1be88b31e8f57bf1fe162a5c1817
Nils Renneberg <nils.renneberg@uni-oldenburg.de>
2023-05-07 12:26:30 +0200

    nav in kleinem Modus angepasst

--- DI-12-B-Visser-Schroeder-Renneberg/README.md
+++ DI-12-B-Visser-Schroeder-Renneberg/README.md
@@ -6,3 +6,7 @@ Namen der Studierenden:
 Abgabe zu Aufgabenblatt 1
 
 Hinweise:
+
+Die Navigation Bar läst sich noch nicht ausklappen, da dazu Skripte nötig währen.
+Für eine Vorschau kann in der nav.php Datei in Zeile 8 und 13 die Klasse open mit zugewissen werden.
+z.B. <ul class="nav-links"> zu <ul class="nav-links open">
--- DI-12-B-Visser-Schroeder-Renneberg/php/nav.php
+++ DI-12-B-Visser-Schroeder-Renneberg/php/nav.php
@@ -2,15 +2,15 @@
 
 <nav>
   <ul class="nav-mobil"> <!-- über java Skript für SimpleToggle-->
-    <li> <a id="nav-links-mobil"> Menu </a> 
-    <li> <a id="nav-rechts-mobil"> Anmelden </a>
+    <li> <a href="#" style="font-size:15px;" class="icon" onclick="">Menu </a> 
+    <li> <a href="#" style="font-size:15px;" class="icon" onclick="">&#9776;</a>
   </ul>
-  <ul class="nav-links open"> <!-- open Tag wird später mit javaSkript gesetzt oder entfernt, garde noch drin damit man sieht wie es später ausgeklappt aussieht -->
+  <ul class="nav-links"> <!-- open Tag wird später mit javaSkript gesetzt oder entfernt, garde noch drin damit man sieht wie es später ausgeklappt aussieht -->
     <li> <a href="index.php">Homepage</a>
     <li> <a href="hauptseite.php">Beiträge</a>
     <li> <a href="eintragneu.php">Neuer Beitrag</a>
   </ul>
-  <ul class="nav-rechts open">
+  <ul class="nav-rechts"> <!-- open Tag wird später mit javaSkript gesetzt oder entfernt, garde noch drin damit man sieht wie es später ausgeklappt aussieht -->
     <li> <a href="anmeldung.php">Anmelden</a>
     <li> <a href="index.php">Abmelden</a>
     <li> <a href="registrieren.php">Registrieren</a>
