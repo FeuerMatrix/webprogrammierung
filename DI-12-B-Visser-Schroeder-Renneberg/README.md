861b58e601b5e2dfb5a0f845145c1ce66ce9636e
Nils Renneberg <nils.renneberg@uni-oldenburg.de>
2023-05-07 13:44:38 +0200

    Bildbeschreibung für hochgeladene Bilder

--- DI-12-B-Visser-Schroeder-Renneberg/README.md
+++ DI-12-B-Visser-Schroeder-Renneberg/README.md
@@ -10,3 +10,10 @@ Hinweise:
 Die Navigation Bar läst sich noch nicht ausklappen, da dazu Skripte nötig währen.
 Für eine Vorschau kann in der nav.php Datei in Zeile 8 und 13 die Klasse open mit zugewissen werden.
 z.B. <ul class="nav-links"> zu <ul class="nav-links open">
+
+
+Später zu beachten:
+
+- Tab Reihenfolge und Tastatur Benutzung für ausgeklappte Navbar muss gut klappen
+- Error-Messages für Form-Elemente mit javaSkript aussagekräftiger machen
+- wenn ein Bild bei einem Beitrag hochgeladen wird, muss eine Beschreibung des Bildes mit andegegben werden.
--- DI-12-B-Visser-Schroeder-Renneberg/eintragneu.php
+++ DI-12-B-Visser-Schroeder-Renneberg/eintragneu.php
@@ -13,8 +13,10 @@
                 <input type="text" id="fname" name="fname" placeholder="Titel" required autofocus><br>
                 <label for="text_main">Text</label> <br>
                 <textarea name="textinput" id="text_main" cols="30" rows="10" placeholder="Beschreibung hier einfügen" required></textarea><br>
-                <label for="files">Bilder auswählen</label>
-                <input type="file" id="files" name="files" accept="image/png, image/jpeg">
+                <label for="files">Bilder auswählen</label><br>
+                <input type="file" id="files" name="files" accept="image/png, image/jpeg"><br>
+                <label for="pname">Bild Beschreibung</label> <br>
+                <input type="text" id="pname" name="pname" placeholder="Bild Beschreibung"><br>
                 <label for="anonym">Anonym</label>
                 <input type="checkbox" id="anonym" name="anonym" value="Anonym"><br>
                 <input type="submit" class="create" value="Erstellen">
