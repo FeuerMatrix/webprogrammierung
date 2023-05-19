2aafadd3cad9e22904d6e08dcec9115a26fd0f32
Robin <robe123@hotmail.de>
2023-05-19 16:04:11 +0200

    bug fixes

--- DI-12-B-Visser-Schroeder-Renneberg/hauptseite.php
+++ DI-12-B-Visser-Schroeder-Renneberg/hauptseite.php
@@ -1,18 +1,17 @@
+<link rel="stylesheet" href="css/hauptseite.css" >
 <?php include_once "php/head.php" ?>
 </head>
 
 <body>
-
     <?php include_once "php/nav.php" ?>
 
-    <main>>
-
+    <main>
         <div class="top">
             <h1>Beiträge</h1>
             <label for="suche">Suche</label>
             <input type="search" placeholder="Hier Suchbegriff eingeben" id="suche" required>
-            <!-- <input type="submit" class="search" value="Suchen">-->
         </div>
+
         <div class="flex-container">
             <?php include "php/einzelBeitrag.php" ?>
             <?php include "php/einzelBeitrag.php" ?>
