ad6ebf5d9efd1993e4e6f6aeccc487c1e9657f3e
Robin <robe123@hotmail.de>
2023-05-31 10:56:57 +0200

    neuer beitrag

--- DI-12-B-Visser-Schroeder-Renneberg/eintragneu.php
+++ DI-12-B-Visser-Schroeder-Renneberg/eintragneu.php
@@ -4,23 +4,22 @@
 <body>
 
     <?php include_once "php/nav.php" ?>
+    <?php include_once "php/beitragErstellen.php" ?>
 
     <main>
 
-        <form>
+        <form method="post" enctype="multipart/form-data">
             <div class="reg">
                 <h1>Neuer Eintrag</h1>
                 <label for="fname">Titel</label> <br>
-                <input type="text" id="fname" name="fname" placeholder="Titel" required autofocus><br>
+                <input type="text"name="fname" placeholder="Titel" required autofocus><br>
                 <label for="text_main">Text</label> <br>
-                <textarea name="textinput" id="text_main" cols="30" rows="10" placeholder="Beschreibung hier einfügen" required></textarea><br>
-                <label for="files">Bilder auswählen</label><br>
-                <input type="file" id="files" name="files" accept="image/png, image/jpeg"><br>
-                <label for="pname">Bild Beschreibung</label> <br>
-                <input type="text" id="pname" name="pname" placeholder="Bild Beschreibung"><br>
+                <textarea name="text_main" cols="30" rows="10" placeholder="Beschreibung hier einfügen" required></textarea><br>
+                <label for="Datei">Bilder auswählen</label><br>
+                <input type="file"name="Datei" accept="image/png, image/jpeg"><br>
                 <label for="anonym">Anonym</label>
-                <input type="checkbox" id="anonym" name="anonym" value="Anonym"><br>
-                <input type="submit" class="create" value="Erstellen">
+                <input type="checkbox" name="anonym" value="Anonym"><br>
+                <input type="submit" name="Submit" class="create" value="Erstellen">
                 <input type="submit" class="cancel" value="Abbrechen">
             </div>
         </form>
---
+++ DI-12-B-Visser-Schroeder-Renneberg/php/beitragErstellen.php
@@ -0,0 +1,42 @@
+<?php
+$titel = (isset($_POST["fname"]) && is_string($_POST["fname"])) ? $_POST["fname"] : "";
+$desc = (isset($_POST["text_main"]) && is_string($_POST["text_main"])) ? $_POST["text_main"] : "";
+$anony = (isset($_POST["anonym"]) && is_string($_POST["anonym"])) ? $_POST["anonym"] : "";
+$titel = htmlspecialchars($titel);
+$anony = htmlspecialchars($anony);
+$desc = nl2br(htmlspecialchars($desc));
+
+
+$ok = false;
+$fehlerfelder = array();
+if (isset($_POST["Submit"])) {
+    $ok = true;
+    if (!isset($_POST["fname"]) || !is_string($_POST["fname"])) {
+        $ok = false;
+        $fehlerfelder[] = "Titel";
+    }
+    if (!isset($_POST["text_main"]) || !is_string($_POST["text_main"]) || trim($_POST["text_main"]) == "") {
+        $ok = false;
+        $fehlerfelder[] = "Beschreibung";
+    }
+    if ($ok) {
+        if (isset($_FILES["Datei"])) { 
+            move_uploaded_file($_FILES["Datei"]["tmp_name"], "../img/" . $_FILES["Datei"]["name"]);
+        }
+
+        if ($anony == "Anonym") {
+            $anony = TRUE;
+        } else {
+            $anony = FALSE;
+        }
+
+        //TODO save to db
+        header("Location: Beitrag.php?from=neuerBeitrag");
+        exit;
+    } else {
+        echo "<p><b>Formular unvollst&auml;ndig</b></p>";
+        echo "<ul><li>";
+        echo implode("</li><li>", $fehlerfelder);
+        echo "</li></ul>";
+    }
+}
