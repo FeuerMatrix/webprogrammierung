0b99a2a06aff56b1e39287db7e8bc307b639fb8a
Robin <robe123@hotmail.de>
2023-05-31 17:22:51 +0200

    eintrag editieren

--- DI-12-B-Visser-Schroeder-Renneberg/eintragneu.php
+++ DI-12-B-Visser-Schroeder-Renneberg/eintragneu.php
@@ -12,15 +12,14 @@
     $database = new DummyUserStore();
 
 
-    if(isset($_POST["Cancel"])){
-       
+    if (isset($_POST["Cancel"])) {
     }
 
-    if(!isset($_GET["from"])){
+    if (!isset($_GET["from"])) {
         $url = "hauptseite.php";
-        }else{
-            $url = "beitrag.php?id=".$_GET["from"];
-        }
+    } else {
+        $url = "beitrag.php?id=" . $_GET["from"];
+    }
 
     if (!isset($_SESSION["user"])) { //Prevents the user from accessing this page through direct links while not logged in
         header("Location: index.php?cause=" . urlencode("Fehler: diese Seite kann nur von eingeloggten Nutzern aufgerufen werden!"));
@@ -49,23 +48,30 @@
 
         <form method="post" enctype="multipart/form-data">
             <div class="reg">
-                <h1>Neuer Eintrag</h1>
+                <h1><?php if (!isset($_GET["from"])) {
+                        echo "Neuer Eintrag";
+                    } else {
+                        echo "Eintrag Editieren";
+                    }
+
+
+                    ?></h1>
                 <label for="fname">Titel</label> <br>
                 <input type="text" id="fname" name="fname" <?php if ($edit) {
-                                                    echo "value=" . $titel;
-                                                } ?> placeholder="Titel" required autofocus><br>
+                                                                echo "value=" . $titel;
+                                                            } ?> placeholder="Titel" required autofocus><br>
                 <label for="text_main">Text</label> <br>
                 <textarea id="text_main" name="text_main" cols="30" rows="10" placeholder="Beschreibung hier einfügen" required> <?php if ($edit) {
-                                                                                                                        echo $desc;
-                                                                                                                    } ?> </textarea><br>
+                                                                                                                                        echo $desc;
+                                                                                                                                    } ?> </textarea><br>
                 <label for="Datei">Bilder auswählen</label><br>
                 <input type="file" id="Datei" name="Datei" accept="image/png, image/jpeg"><br>
                 <label for="anonym">Anonym</label>
                 <input type="checkbox" id="anonym" name="anonym" value="Anonym" <?php if ($edit && $anony) {
-                                                                        echo 'checked="checked"';
-                                                                    } ?>><br>
+                                                                                    echo 'checked="checked"';
+                                                                                } ?>><br>
                 <input type="submit" name="Submit" class="create" value="Erstellen">
-                <input type="submit" form="form" name="Cancel" class="cancel" value="Abbrechen" >
+                <input type="submit" form="form" name="Cancel" class="cancel" value="Abbrechen">
             </div>
         </form>
 
