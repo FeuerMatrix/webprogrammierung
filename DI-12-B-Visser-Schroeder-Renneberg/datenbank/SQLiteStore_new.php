a82c1acd31971d7d4f6b47f632f1cb7fa5610134
Nils Renneberg <nils.renneberg@uni-oldenburg.de>
2023-06-04 17:27:57 +0200

    Datenbankstruktur angepasst

--- DI-12-B-Visser-Schroeder-Renneberg/datenbank/SQLiteStore_new.php
+++ DI-12-B-Visser-Schroeder-Renneberg/datenbank/SQLiteStore_new.php
@@ -2,13 +2,40 @@
 
     $db = new SQLite3( 'beschwerdeforum.db' );
 
+    //NUTZER TABELLE
+    $sql = "CREATE TABLE IF NOT EXISTS nutzer (
+        id_nutzer   INT PRIMARY KEY,
+        email       TEXT NOT NULL,
+        passwort    TEXT NOT NULL
+        )";
+
+        if ( $db->exec( $sql ) ) {
+            echo 'Tabelle Nutzer vorhanden.<br />';
+        } else {
+            echo 'Fehler beim Anlegen der Nutzer-Tabelle!<br />';
+        }
+
+    $sql = "INSERT OR IGNORE INTO nutzer VALUES (
+        0 , 'tim@test.de', 'helloworld'
+        )";
+
+        if ( $db->exec( $sql ) ) {
+            echo "Erste Nutzer-Daten sind vorhanden<br />";
+    
+        } else {
+            echo 'Fehler beim anlegen der ersten Nutzerdaten!';
+        }
+
     //BEITRAGS TABELLE
     $sql = "CREATE TABLE IF NOT EXISTS beitrag (
         id_beitrag          INTEGER PRIMARY KEY,
-        titel               TEXT,
-        datum               TIMESTAMP,
+        author              INT,
+        anonym              BOOLEAN,
+        titel               TEXT NOT NULL,
+        datum               TIMESTAMP,  
         bild                TEXT,
-        bildbeschreibung    TEXT
+        bildbeschreibung    TEXT,
+        FOREIGN KEY(author) REFERENCES nutzer(nutzername)
         )";
 
         if ( $db->exec( $sql ) ) {
@@ -18,10 +45,10 @@
         }    
 
     $sql = "INSERT OR IGNORE INTO beitrag VALUES
-        (1, 'Argumentation', '05.06.1996', 'images/beispielbilder/argumentation.png', 'Argumentation'),
-        (2, 'Protest', '08.04.1976', 'images/beispielbilder/protest.png', 'Protest'),
-        (3, 'Trouble Incoming', '08.05.1976', 'images/beispielbilder/trouble.jpg', 'Trouble_Schilder'),
-        (4, 'Beispielbild', '08.04.1976', 'images/guestbook.png', 'Beispielbild'
+        (1, 0, FALSE, 'Argumentation', '05.06.1996', 'images/beispielbilder/argumentation.png', 'Argumentation'),
+        (2, 0, FALSE, 'Protest', '08.04.1976', 'images/beispielbilder/protest.png', 'Protest'),
+        (3, 0, FALSE, 'Trouble Incoming', '08.05.1976', 'images/beispielbilder/trouble.jpg', 'Trouble_Schilder'),
+        (4, 0, FALSE, 'Beispielbild', '08.04.1976', 'images/guestbook.png', 'Beispielbild'
         )";
 
         if ( $db->exec( $sql ) ) {
@@ -33,12 +60,13 @@
 
     //KOMMENTAR TABELLE
     $sql = "CREATE TABLE IF NOT EXISTS kommentar (
-        id_kommentar   INTEGER,
+        id_kommentar    INTEGER,
         id_beitrag      INTEGER,
-        author          TEXT,
+        author          INT,
         kommentar       TEXT,
         PRIMARY KEY(id_beitrag, id_kommentar),
-        FOREIGN KEY(id_beitrag) REFERENCES beitrag(id_beitrag)
+        FOREIGN KEY(id_beitrag) REFERENCES beitrag(id_beitrag),
+        FOREIGN KEY(author) REFERENCES nutzer(nutzername)
         )";
 
         if ( $db->exec( $sql ) ) {
@@ -48,39 +76,15 @@
         }
 
     $sql = "INSERT OR IGNORE INTO kommentar VALUES
-        (1, 1, 'Tim','grfaghaergherreag'),
-        (1, 2, 'Nils','harhngrjrtahgrfsjhtr'),
-        (1, 3, 'Robin','htrshtgbgsfhjtrsh')";
+        (1, 1, 0,'grfaghaergherreag'),
+        (1, 2, 0,'harhngrjrtahgrfsjhtr'),
+        (1, 3, 0,'htrshtgbgsfhjtrsh')";
 
         if ( $db->exec( $sql ) ) {
             echo "Erste Kommentar-Daten sind vorhanden<br />";
 
         } else {
             echo 'Fehler beim anlegen der ersten Kommentardaten!';
-        }
-
-    //NUTZER TABELLE
-    $sql = "CREATE TABLE IF NOT EXISTS nutzer (
-        nutzername  TEXT PRIMARY KEY,
-        email       TEXT,
-        passwort    TEXT
-        )";
-
-        if ( $db->exec( $sql ) ) {
-            echo 'Tabelle Nutzer vorhanden.<br />';
-        } else {
-            echo 'Fehler beim Anlegen der Nutzer-Tabelle!<br />';
-        }
-
-    $sql = "INSERT OR IGNORE INTO nutzer VALUES (
-        'tim', 'tim@test.de', 'helloworld'
-        )";
-
-        if ( $db->exec( $sql ) ) {
-            echo "Erste Nutzer-Daten sind vorhanden<br />";
-    
-        } else {
-            echo 'Fehler beim anlegen der ersten Nutzerdaten!';
         }
         
 ?>
