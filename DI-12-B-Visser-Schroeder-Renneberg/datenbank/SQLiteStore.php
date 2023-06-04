ebb0646663975bbda6bdf091fe1677313334fcd8
Robin <robe123@hotmail.de>
2023-06-04 17:12:18 +0200

    removed

--- DI-12-B-Visser-Schroeder-Renneberg/datenbank/SQLiteStore.php
+++ DI-12-B-Visser-Schroeder-Renneberg/datenbank/SQLiteStore.php
@@ -1,6 +1,6 @@
 <?php
-    include_once "datenbank/UserStore.php";
-    include_once "datenbank/SQLiteStore_new.php";
+    include_once "UserStore.php";
+    include_once "SQLiteStore_new.php";
 
     class SQLiteStore implements UserStore {
 //pw hashen
