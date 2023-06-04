2db468e6e59dbee9359c8f447da58f27562ba8d3
Nils Renneberg <nils.renneberg@uni-oldenburg.de>
2023-06-04 13:46:11 +0200

    Funktion für die SQLite DB Schnittstelle, Funktionen sind noch leer

---
+++ DI-12-B-Visser-Schroeder-Renneberg/datenbank/SQLiteStore.php
@@ -0,0 +1,33 @@
+<?php
+
+
+    class SQLiteStore implements UserStore {
+
+
+        function store($user, $email, $pw){
+
+        }
+
+        function checkLoginData($email, $pw){
+
+        }
+
+        function isLoggedIn($email){
+
+        }
+
+        function userNameExists($username){
+
+        }
+
+        function getUser($user){
+
+        }
+
+        function getBeitraege(){
+
+        }
+
+    }
+
+?>
