91d9af10ea0fe880a4ec3dc3f8949bd15ec9b62b
Robin <robe123@hotmail.de>
2023-06-07 17:35:27 +0200

    a

--- DI-12-B-Visser-Schroeder-Renneberg/php/controller_beitragErstellen.php
+++ DI-12-B-Visser-Schroeder-Renneberg/php/controller_beitragErstellen.php
@@ -40,7 +40,7 @@ if (isset($_POST["Submit"])) {
         }
         $database->newPost($_SESSION["user"], $titel, $desc, $anony,  "./images/userImages/" . $_FILES["Datei"]["name"]);
         if ($edit) {
-            $database->updatePost($id, $_SESSION["user"], $titel, $desc, $anony, "img");
+            $database->updatePost($id, $_SESSION["user"], $titel, $desc, $anony, "./images/userImages/" . $_FILES["Datei"]["name"]);
         }
         header("Location: Beitrag.php?from=neuerBeitrag");
         exit;
