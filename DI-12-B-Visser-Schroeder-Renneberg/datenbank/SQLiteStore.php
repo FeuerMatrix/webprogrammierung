7f5e507f087d49b5769e7d21f4c490d35b5e06a2
Nils Renneberg <nils.renneberg@uni-oldenburg.de>
2023-06-07 12:12:05 +0200

    DB [ber PDO eingebunden und einzelne  Funktionen der SQLiteDB angepasst

--- DI-12-B-Visser-Schroeder-Renneberg/datenbank/SQLiteStore.php
+++ DI-12-B-Visser-Schroeder-Renneberg/datenbank/SQLiteStore.php
@@ -6,95 +6,101 @@
 //Transaction (registrieren)
         protected $db;
         public function __construct(){
-            $this->db = new SQLite3( 'beschwerdeforum.db' );
+            try{
+                $dsn = 'sqlite:sqlite-beschwerdeforum.db';
+                $this->db = new PDO($dsn);
 
-            //Creates Tables and fills them with dummy data.
-            //TODO remove dummy data
+                //Creates Tables and fills them with dummy data.
+                //TODO remove dummy data
 
-            //NUTZER TABELLE
-            $sql = "CREATE TABLE IF NOT EXISTS nutzer (
-                id_nutzer   INT PRIMARY KEY,
-                email       TEXT NOT NULL,
-                passwort    TEXT NOT NULL
-            )";
+                //NUTZER TABELLE
+                $sql = "CREATE TABLE IF NOT EXISTS nutzer (
+                    id_nutzer   INT PRIMARY KEY,
+                    email       TEXT NOT NULL,
+                    passwort    TEXT NOT NULL
+                )";
 
-            if ( $this->db->exec( $sql ) ) {
-            } else {
-                echo 'Fehler beim Anlegen der Nutzer-Tabelle!<br />';
-            }
+                if ( $this->db->exec( $sql ) !== false ) {
+                } else {
+                    echo 'Fehler beim Anlegen der Nutzer-Tabelle!<br />';
+                }
 
-            $pw = password_hash('helloworld',PASSWORD_DEFAULT);
-            $sql = "INSERT OR IGNORE INTO nutzer VALUES (
-                0 , 'tim@test.de', $pw
-            )";
+                $pw = password_hash('helloworld',PASSWORD_DEFAULT);
+                $sql = "INSERT OR IGNORE INTO nutzer VALUES (
+                    0 , 'tim@test.de', '$pw'
+                )";
 
-            if ( $this->db->exec( $sql ) ) {
-            } else {
-                echo 'Fehler beim anlegen der ersten Nutzerdaten!';
-            }
+                if ( $this->db->exec( $sql ) !== false ) {
+                } else {
+                    echo 'Fehler beim anlegen der ersten Nutzerdaten!<br />';
+                }
 
-            //BEITRAGS TABELLE
-            $sql = "CREATE TABLE IF NOT EXISTS beitrag (
-                id_beitrag          INTEGER PRIMARY KEY,
-                author              INT,
-                anonym              BOOLEAN NOT NULL,
-                titel               TEXT NOT NULL,
-                datum               TIMESTAMP,  
-                bild                TEXT,
-                beschreibung    TEXT,
-                FOREIGN KEY(author) REFERENCES nutzer(nutzername)
-            )";
+                //BEITRAGS TABELLE
+                $sql = "CREATE TABLE IF NOT EXISTS beitrag (
+                    id_beitrag          INTEGER PRIMARY KEY,
+                    author              INT,
+                    anonym              BOOLEAN NOT NULL,
+                    titel               TEXT NOT NULL,
+                    datum               TIMESTAMP,  
+                    bild                TEXT,
+                    beschreibung    TEXT,
+                    FOREIGN KEY(author) REFERENCES nutzer(nutzername)
+                )";
 
-            if ( $this->db->exec( $sql ) ) {
-            } else {
-                echo 'Fehler beim Anlegen der Beitrags-Tabelle!<br />';
-            }    
+                if ( $this->db->exec( $sql ) !== false ) {
+                } else {
+                    echo 'Fehler beim Anlegen der Beitrags-Tabelle!<br />';
+                }    
 
-            $sql = "INSERT OR IGNORE INTO beitrag VALUES
-                (1, 0, FALSE, 'Argumentation', '05.06.1996', 'images/beispielbilder/argumentation.png', 'Argumentation'),
-                (2, 0, FALSE, 'Protest', '08.04.1976', 'images/beispielbilder/protest.png', 'Protest'),
-                (3, 0, FALSE, 'Trouble Incoming', '08.05.1976', 'images/beispielbilder/trouble.jpg', 'Trouble_Schilder'),
-                (4, 0, FALSE, 'Beispielbild', '08.04.1976', 'images/guestbook.png', 'Beispielbild'
-            )";
+                $sql = "INSERT OR IGNORE INTO beitrag VALUES
+                    (1, 0, FALSE, 'Argumentation', '05.06.1996', 'images/beispielbilder/argumentation.png', 'Argumentation'),
+                    (2, 0, FALSE, 'Protest', '08.04.1976', 'images/beispielbilder/protest.png', 'Protest'),
+                    (3, 0, FALSE, 'Trouble Incoming', '08.05.1976', 'images/beispielbilder/trouble.jpg', 'Trouble_Schilder'),
+                    (4, 0, FALSE, 'Beispielbild', '08.04.1976', 'images/guestbook.png', 'Beispielbild'
+                )";
 
-            if ( $this->db->exec( $sql ) ) {
+                if ( $this->db->exec( $sql ) !== false ) {
 
-            } else {
-                echo 'Fehler beim anlegen der ersten Beitragsdaten!';
-            }
+                } else {
+                    echo 'Fehler beim anlegen der ersten Beitragsdaten!<br />';
+                }
 
-            //KOMMENTAR TABELLE
-            $sql = "CREATE TABLE IF NOT EXISTS kommentar (
-                id_kommentar    INTEGER,
-                id_beitrag      INTEGER,
-                author          INT,
-                kommentar       TEXT,
-                PRIMARY KEY(id_beitrag, id_kommentar),
-                FOREIGN KEY(id_beitrag) REFERENCES beitrag(id_beitrag),
-                FOREIGN KEY(author) REFERENCES nutzer(nutzername)
-            )";
+                //KOMMENTAR TABELLE
+                $sql = "CREATE TABLE IF NOT EXISTS kommentar (
+                    id_kommentar    INTEGER,
+                    id_beitrag      INTEGER,
+                    author          INT,
+                    kommentar       TEXT,
+                    PRIMARY KEY(id_beitrag, id_kommentar),
+                    FOREIGN KEY(id_beitrag) REFERENCES beitrag(id_beitrag),
+                    FOREIGN KEY(author) REFERENCES nutzer(nutzername)
+                )";
 
-            if ( $this->db->exec( $sql ) ) {
-            } else {
-                echo 'Fehler beim Anlegen der Kommentar-Tabelle!<br />';
-            }
+                if ( $this->db->exec( $sql ) !== false ) {
+                } else {
+                    echo 'Fehler beim Anlegen der Kommentar-Tabelle!<br />';
+                }
 
-            $sql = "INSERT OR IGNORE INTO kommentar VALUES
-                (1, 1, 0,'grfaghaergherreag'),
-                (1, 2, 0,'harhngrjrtahgrfsjhtr'),
-                (1, 3, 0,'htrshtgbgsfhjtrsh'
-            )";
+                $sql = "INSERT OR IGNORE INTO kommentar VALUES
+                    (1, 1, 0,'grfaghaergherreag'),
+                    (1, 2, 0,'harhngrjrtahgrfsjhtr'),
+                    (1, 3, 0,'htrshtgbgsfhjtrsh'
+                )";
 
-            if ( $this->db->exec( $sql ) ) {
-            } else {
-                echo 'Fehler beim anlegen der ersten Kommentardaten!';
+                if ( $this->db->exec( $sql ) !== false ) {
+                } else {
+                    echo 'Fehler beim anlegen der ersten Kommentardaten!<br />';
+                }
+             } catch ( PDOException $e ) {
+                echo 'Fehler: ' . htmlspecialchars( $e->getMessage() );
+                exit();
             }
         }
 
         // Speichert den Nutzer ein oder Updatet ihn
         function store($user, $email, $pw){
             try {
-                $sql = "INSERT OR UPDATE nutzer (id_nutzer, email, passwort) VALUES (? , ?, ?)";
+                $sql = "INSERT OR REPLACE nutzer (id_nutzer, email, passwort) VALUES (? , ?, ?)";
                 $stmt = $this->db->prepare($sql);
                 $stmt->bind_param("iss", $user, $email,password_hash($pw,PASSWORD_DEFAULT));
                 $stmt->execute();
@@ -106,10 +112,14 @@
         // überprüft Einlogdaten des Nutzer
         function checkLoginData($email, $pw){
             try {
-                $sql = "SELECT (id_nutzer) FROM nutzer WHERE email = ? AND passwort = ?";
-                $stmt = $this->db->prepare($sql);
-                $stmt->bind_param("ss", $email,password_hash($pw,PASSWORD_DEFAULT)); 
-                return $stmt->execute();
+                $sql = "SELECT (id_nutzer) FROM nutzer WHERE email = ?";
+                $stmt = $this->db->prepare($sql); 
+                $stmt->bind_param("s", $email); 
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $row = $result->fetch_assoc();
+                $storedPassword = $row['passwort'];
+                return password_verify($pw, $storedPassword);
             } catch (Exception $ex) {
                 echo "Fehler: " . $ex->getMessage();
             }
@@ -119,7 +129,7 @@
         function isLoggedIn($email){
             try {
                 $sql = "SELECT (id_nutzer) FROM nutzer WHERE email = ?";
-                $stmt = $db->prepare($sql);
+                $stmt = $this->db->prepare($sql);
                 $stmt->bind_param("s", $email); 
                 return $stmt->execute();
             } catch (Exception $ex) {
@@ -154,8 +164,8 @@
         function getBeitraege(){
             try {
                 $sql = "SELECT * FROM beitrag";
-                $ergebnis = $this->db->query($sql);
-                $array = $ergebnis->fetchAll();
+                $stmt = $this->db->query($sql);
+                $originalArray = $stmt->fetchAll(PDO::FETCH_ASSOC);
             } catch (Exception $ex) {
                 echo "Fehler: " . $ex->getMessage();
                 return [];
@@ -167,7 +177,7 @@
                 $newItem = array(
                     'id' => $item['id_beitrag'],
                     'author' => $item['author'],
-                    'anonym' => $item['ananoym'],
+                    'anonym' => $item['anonym'],
                     'titel' => $item['titel'],
                     'date' => $item['datum'],
                     'file' => $item['bild'],
