fbafa719727786879d29db6ff459a0b2d9c0f1de
Nils Renneberg <nils.renneberg@uni-oldenburg.de>
2023-06-18 11:16:56 +0200

    ajax suche und sortierung in hauptseite eingebunden

--- DI-12-B-Visser-Schroeder-Renneberg/hauptseite.php
+++ DI-12-B-Visser-Schroeder-Renneberg/hauptseite.php
@@ -1,7 +1,42 @@
 <?php include_once "php/controller_hauptseite.php" ?>
-
 <?php include_once "php/head.php" ?>
 <link rel="stylesheet" href="css/hauptseite.css" >
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script>
+$(document).ready(function() {
+    $("#suche, #sort").on("input change", function() {
+        var search = $("#suche").val(); // Lese den Suchbegriff aus dem Eingabefeld
+        var sort = $("#sort").val(); // Lese den Wert der Sortierungs-Option aus dem Dropdown-Menü
+
+        // Sende Ajax-Anfrage an den Server
+        $.ajax({
+            type: "GET",
+            url: "php/search.php",
+            async: true,
+            data: { search: search, sort: sort },
+            dataType: "json",
+            success: function(data) {
+                var container = $(".flex-container");
+                container.empty(); // Leere den Inhalt der flex-container
+
+                // erstelle neue Beitragselemente
+                $.each(data, function(index, beitrag) {
+                    var beitragDiv = $("<div>").addClass("beitrag");
+                    var link = $("<a>").addClass("link").attr("href", "beitrag.php?id=" + encodeURIComponent(beitrag.id)).text(beitrag.titel);
+                    var dateSpan = $("<span>").text(new Date(beitrag.date * 1000).toLocaleString());
+                    var img = $("<img>").attr("src", beitrag.file).attr("alt", beitrag.pname);
+
+                    beitragDiv.append(link, dateSpan, img);
+                    container.append(beitragDiv);
+                });
+            }
+        });
+    });
+});
+</script>
+
+
 </head>
 
 <body>
@@ -12,13 +47,12 @@
             <h1>Beiträge</h1>
             <form> 
                 <label for="suche">Suche</label> <br>
-                <input type="search" name="suche" placeholder="Hier Suchbegriff eingeben" id="suche"> <br>
+                <input id="suche" type="search" name="suche" placeholder="Hier Suchbegriff eingeben"> <br>
                 <label for="sort">Sortieren nach:</label> <br>
                 <select id="sort" name="sort">
                     <option value="date">Datum</option>
                     <option value="titel">Alphabetisch</option>
                 </select> <br>
-                <input type="submit" value="Auswahl bestätigen">
             </form>
         </div>
 
