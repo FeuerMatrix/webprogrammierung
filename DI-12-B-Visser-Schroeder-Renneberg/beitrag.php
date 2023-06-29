599ffe8c91107ab872f3741ee57a2e573fd843f7
Robin <robe123@hotmail.de>
2023-06-29 18:21:23 +0200

    dont show map when not logged in

--- DI-12-B-Visser-Schroeder-Renneberg/beitrag.php
+++ DI-12-B-Visser-Schroeder-Renneberg/beitrag.php
@@ -21,7 +21,17 @@
                         } ?></h2>
                     <h2><?php echo $date ?></h2>
                 </div>
-                <div id="map"></div>
+                <?php
+                if (isset($_SESSION["user"])) {
+                ?>
+                    <div id="map"></div>
+                <?php
+                } else if($lat!="'.'"){
+                    ?>
+                    <p>Melde dich an um die Karte zu sehen</p>
+                    <?php
+                }
+                ?>
                 <p class=center><?php echo $desc ?> </p>
                 <?php if (isset($_SESSION["user"]) && $_SESSION["user"] == $database->getAuthor($id)) : ?>
                     <form method="post">
@@ -69,16 +79,22 @@
 
 
 <script>
-    var lat = <?php print($lat); ?>;
-    var lng = <?php print($lng); ?>;
-    if (lat != '.' && lng != '.') {
-        var map = L.map('map').setView([53.146962, 8.182063], 13);
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
-        var marker = L.marker([lat, lng]).addTo(map);
-    } else {
-        document.getElementById("map").remove();
+    <?php
+    if (isset($_SESSION["user"])) {
+    ?>
+        var lat = <?php print($lat); ?>;
+        var lng = <?php print($lng); ?>;
+        if (lat != '.' && lng != '.') {
+            var map = L.map('map').setView([53.146962, 8.182063], 13);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+            var marker = L.marker([lat, lng]).addTo(map);
+        } else {
+            document.getElementById("map").remove();
+        }
+    <?php
     }
+    ?>
 </script>
