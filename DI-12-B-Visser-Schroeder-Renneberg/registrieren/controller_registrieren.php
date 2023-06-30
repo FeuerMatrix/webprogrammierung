3de276031a3485e0c9c35999b86f5d080f21a781
Nils Renneberg <nils.renneberg@uni-oldenburg.de>
2023-06-30 22:25:24 +0200

    Wenn alle Daten im Registrieren Fenster richtig sind, wird eine Email geschickt und auf diese weitergeleitet

--- DI-12-B-Visser-Schroeder-Renneberg/registrieren/controller_registrieren.php
+++ DI-12-B-Visser-Schroeder-Renneberg/registrieren/controller_registrieren.php
@@ -29,11 +29,11 @@
         $database = new SQLiteStore();
         $token = crypt($email, $salt);
         if($database->emailExists($email)) {
-            $errorMessage = "Fehler!";
             $emailLog = fopen("email.txt", "w");
             $linkPWResset = "http://localhost/webprogrammierung/DI-12-B-Visser-Schroeder-Renneberg/pwReset.php?token=".$token;
             fwrite($emailLog,  "Bitte ignoriere die E-Mail, wenn du es nicht warst, \nder sich versucht hat zu registrieren. \nDu bist aber bereits registriert. \nSolltest du dein Password vergessen haben, klicke auf folgenden Link. \n$linkPWResset");
             fclose($emailLog);
+            header("Location: registrierenFertig.php");
         }
 
         if(!isset($errorMessage)) {
@@ -42,7 +42,7 @@
             $linkRegestrierung = "http://localhost/webprogrammierung/DI-12-B-Visser-Schroeder-Renneberg/confirmEmail.php?token=".$token;
             fwrite($emailLog,  "Bitte ignoriere die E-Mail, wenn du es nicht warst, \nder sich versucht hat zu registrieren. \nAnsonsten klicke innerhalb von 24h auf den folgenden Link, um die Registrierung abzuschließen: \n$linkRegestrierung");
             fclose($emailLog);
-            header("Location: anmeldung.php?from=registration");
+            header("Location: registrierenFertig.php");
             exit;
         } else {
             header("Location: registrieren.php?cause=".urlencode($errorMessage)."&email=".$email."&email2=".$email2);
