38d00688410a56e7fe7af29a5e044e2e26576531
Nils Renneberg <nils.renneberg@uni-oldenburg.de>
2023-06-30 22:02:43 +0200

    Funktion für Passwort Reset eingebaut

--- DI-12-B-Visser-Schroeder-Renneberg/registrieren/controller_pwReset.php
+++ DI-12-B-Visser-Schroeder-Renneberg/registrieren/controller_pwReset.php
@@ -1,4 +1,36 @@
 <?php
 
+    if(isset($_GET["token"])){
+        if(isset($_POST["passw"], $_POST["passw2"])){
+            
+            if($_POST["passw"] != $_POST["passw2"]) {
+                $errorMessage = "Passwörter stimmen nicht überein!";
+            }
+
+            if(!isset($errorMessage)) {
+                
+                    include_once "datenbank/SQLiteStore.php";
+                    $database = new SQLiteStore();
+                    $user = $database->getUser($_GET["token"]);
+
+                    if($user != "") {
+                        $database->updatePassword($user, $_POST["passw"]);
+                        $errorMessage = "Passwort erfolgreich zurückgesetzt";
+                        header("Location: index.php?cause=".urlencode($errorMessage));
+                    } 
+                    else {
+                        $errorMessage = "Keinen Nutzer gefunden!";
+                        header("Location: pwReset.php?cause=".urlencode($errorMessage)."&token=".$token);
+                    }     
+            } 
+            else {
+                header("Location: pwReset.php?cause=".urlencode($errorMessage)."&token=".$_GET["token"]);
+            }
+        }
+    } 
+    else {
+        $errorMessage = "Fehlerhafte URL";
+        header("Location: index.php?cause=".urlencode($errorMessage));
+    }
 
 ?>
