52a9dadaf2b84ebab77bcb3db1e2e25fd858cccd
FireMatrix <tim.schroeder02@gmx.de>
2023-07-04 13:24:42 +0200

    more useless code

--- DI-12-B-Visser-Schroeder-Renneberg/javascript/error_popup.php
+++ DI-12-B-Visser-Schroeder-Renneberg/javascript/error_popup.php
@@ -2,7 +2,6 @@
   const s = new URLSearchParams(location.search).get("cause");
     if (s != null) {
         document.getElementById("errorbox").innerHTML = s;
-          document.getElementById("errorpopup").className = "errorpopup";
         setTimeout((() => {
           document.getElementById("errorbox").innerHTML = "";
           document.getElementById("errorpopup").className = "";
