963eb14610139102bb27905bb553bc8fa30935bc
FireMatrix <tim.schroeder02@gmx.de>
2023-07-04 13:23:15 +0200

    removed useless code

--- DI-12-B-Visser-Schroeder-Renneberg/javascript/error_popup.php
+++ DI-12-B-Visser-Schroeder-Renneberg/javascript/error_popup.php
@@ -1,6 +1,5 @@
 <script>
   const s = new URLSearchParams(location.search).get("cause");
-  const e = document.getElementById("errorpopup");
     if (s != null) {
         document.getElementById("errorbox").innerHTML = s;
           document.getElementById("errorpopup").className = "errorpopup";
