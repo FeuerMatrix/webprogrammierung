168bd9021e5d117cb811f3483ab7d798fb2895d7
Robin <robe123@hotmail.de>
2023-07-05 16:10:16 +0200

    .

--- DI-12-B-Visser-Schroeder-Renneberg/drittAnbieter.php
+++ DI-12-B-Visser-Schroeder-Renneberg/drittAnbieter.php
@@ -1,30 +1,55 @@
+<?php include_once "php/head.php" ?>
+<link rel="stylesheet" href="css/index.css">
+</head>
+
+<body>
+
+    <?php include_once "php/nav.php" ?>
+
+    <main>
+
+        <?php
+        echo $_COOKIE["accept"];
+        $set = isset($_COOKIE["accept"]);
+
+        if (!$set) :
+        ?>
+            <form method="post">
+                <input type="hidden" id="acc" name="acc" value="1">
+                <input type="submit" name="accept" class="navdelete" value="Map Datenschutz zustimmen">
+            </form>
+        <?php
+        endif;
+        ?>
+
+        <?php
+        if ($set) :
+        ?>
+            <form method="post">
+                <input type="hidden" id="notacc" name="notacc" value="1">
+                <input type="submit" name="accept" class="navdelete" value="Map Datenschutz nicht zustimmen">
+            </form>
+        <?php
+        endif;
+        ?>
+
+
+    </main>
+
+    <?php include_once "php/footer.php" ?>
+</body>
+
+</html>
+
 <?php
-      if (!isset($_SESSION["accept"])) :
-      ?>
-      <form method="post">
-        <input type="submit" name="accept"  class="navdelete" value="Map Datenschutz zustimmen">
-      </form>
-    <?php
-      endif;
-    ?>
+if (isset($_POST["acc"])) {
+    echo "set";
+    setcookie("accept", "set", time() + (86400 * 30), "/"); // 86400 = 1 day
+}
 
-    <?php
-    if (isset($_SESSION["accept"])) :
-    ?>
-      <form method="post">
-        <input type="submit" name="accept" class="navdelete" value="Map Datenschutz nicht zustimmen">
-      </form>
-    <?php
-    endif;
-    ?>
-
-<?php
-if (isset($_POST["accept"])) {
-
-  if (!isset($_SESSION["accept"])) {
-    $_SESSION["accept"] = "set";
-  } else {
-    unset($_SESSION["accept"]);
-  }
+if (isset($_POST["notacc"])) {
+    echo "notset";
+    unset($_COOKIE["accept"]);
+    setcookie("accept", "", time() - 3600);
 }
 ?>
