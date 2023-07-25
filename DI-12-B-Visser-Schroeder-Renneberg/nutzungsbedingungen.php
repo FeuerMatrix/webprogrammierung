198ca66cb276eccba56c1c784b48856244f20ef4
Nils Renneberg <nils.renneberg@uni-oldenburg.de>
2023-07-25 12:18:43 +0200

    Nutzungsbedingungen

--- DI-12-B-Visser-Schroeder-Renneberg/nutzungsbedingungen.php
+++ DI-12-B-Visser-Schroeder-Renneberg/nutzungsbedingungen.php
@@ -7,35 +7,53 @@
 
     <main>
 
-        <h1>Nutzungsbedingungen</h1>
-        <p>
-            Mit Nutzung dieser Webseite erklären sie ihr Einverständnis zu den folgenden Punkten:
-        </p>
-
-        <h2>Hinweise für die Nutzung des Beschwerdeforums</h2>
-        <p>
-            Beachten Sie bitte im Vorfeld die vorhandenen Beiträge. Vielleicht wurde der von Ihnen beschriebene Beschwerde bereits von anderen beschrieben.<br>
-            Das System dient dem Austausch des Umganges mit Fehlverhaltens und Handlungsalternativen und ist nicht für das öffentliche Anzeigen von Mitbürgern vorgesehen. Daher werden Meldungen die persönliche Daten beinhalten, nicht veröffentlicht.<br>
-        </p>
-
+        <p>&nbsp;</p>
+        <h4><span style="text-decoration: underline;">Besondere Nutzungsbedingungen für die Forenregistrierung im </span><span style="text-decoration: underline;">(Beschwerdeforum)</span><span style="text-decoration: underline;"> -Forum:</span></h4>
+        <p>&nbsp;</p><p><strong>§ 1 Geltungsbereich</strong></p>
+        <p>(1)&nbsp;Die nachfolgenden Bedingungen gelten für die Nutzung des Beschwerdeforums - im folgenden "Unsere Webseite" genannt - Forums.&nbsp;Für die Nutzung des Forums ist wichtig, dass Sie als Nutzer/Nutzerin die nachfolgenden Forenregeln bzw. -bedingungen akzeptieren. Die Registrierung und Benutzung unseres Forums ist kostenlos.</p>
+        <p>(2)&nbsp;Mit der Registrierung sind Sie mit den Nutzungsbedingungen unserer Webseite einverstanden. Durch Ihr Einverständnis garantieren Sie uns, dass Sie keine Beiträge erstellen werden, die gegen die Nutzungsbedingung verstoßen.</p>
+        <p>(3) Durch das Benutzen von unserer Webseite kommt kein Vertrag zwischen den Nutzern/Nutzerinnen und uns zustande.</p>
+        <p><strong>&nbsp;</strong></p>
+        <p><strong>§ 2 Pflichten als Forum-Nutzer/Forum-Nutzerinnen</strong></p>
+        <p><sup>&nbsp;</sup>(1) Einer Ihrer Pflichten als Nutzer/Nutzerin ist es, dass Sie keine Beiträge veröffentlichen, die gegen diese Forenregeln, gegen die guten Sitten oder sonst gegen geltendes deutsches Recht verstoßen.</p>
+        <p><span style="text-decoration: underline;">Folgende Punkte sind Ihnen nicht genehmigt:</span></p>
+        <p>1. Inhalte zu veröffentlichen, die unwahr sind und deren Veröffentlichung einen Straftatbestand oder eine Ordnungswidrigkeit erfüllt,<br />2. Versendung von Spam über das Forum an andere Forum-Nutzer/Form-Nutzerin,<br />3. Verwendung von gesetzlich durch Urheber- und Markenrecht geschützte Inhalte ohne rechtmäßige Berechtigung (z.B. Pressemitteilungen etc.),<br />4. Handlungen, die wettbewerbswidrig sind,<br />5. mehrfache Einstellung von Themen ins Forum (sogenannte Doppelpostings),<br />6. eigene Werbung, folglich Schleichwerbung, zu betreiben und<br />7. Inhalte zu veröffentlichen, die beleidigend, rassistisch, diskriminierend oder pornographische Elemente aufweisen gegenüber anderen Nutzern/Nutzerinnen und der Allgemeinheit.</p>
+        <p>Ihre Pflicht als Forum-Nutzer/ Forum-Nutzerin ist es, auf § 2 Abs. 1 Nr. 1-7 dieser Nutzungsbedingung, vor&nbsp; der Veröffentlichung Ihres Beitrages im Forum zu beachten und zu überprüfen, ob Sie sich an diese Punkte gehalten haben.</p>
+        <p>(2)&nbsp;&nbsp;&nbsp;Sollten Sie gegen § 2 Abs. 1 Nr. 1-7 dieser Nutzungsbedingung verstoßen, behalten wir uns das Recht vor, gegen Sie folgende Schritte vorzunehmen:</p><p>1. Ihre eingestellten Beiträge zu löschen und diese abzuändern,</p>
+        <p>2. Verbot weiterhin im Forum Beiträge zu verfassen und</p>
+        <p>3. Sperrung des Zugangs als Nutzer/Nutzerin.</p>
+        <p>(3)&nbsp;&nbsp;Haben Sie als Forum-Nutzer/Forum-Nutzerin nicht die Forenregeln beachtet und sind hierdurch mögliche Rechtsverstöße entstanden, die durch Ihre eingestellten Inhalte in unserem Forum entstanden sind (Pflichtverletzung), verpflichten Sie sich als Nutzer/Nutzerin, <em>uns </em>von jeglichen Ansprüchen, auch den Schadenersatzansprüchen, freizustellen und diesen die Kosten zu ersetzen.</p>
+        <p>Zudem ist der Nutzer/die Nutzerin verpflichtet uns, bei Schadenersatzansprüchen hinsichtlich der Abwehr des durch ihn entstandenen Rechtsverstoßes (Pflichtverletzung s.o.), zu unterstützen und die Kosten einer angemessenen Rechtsverteidigung für uns zu tragen.</p>
+        <p>(4)&nbsp;&nbsp; Durch Ihr Einverständnis garantieren Sie uns, dass Sie keine Beträge erstellen werden, die gegen die Nutzungsbedingung verstoßen. Entsprechendes gilt auch für das Setzen von externen Links und die Signaturen.</p>
+        <p>&nbsp;</p>
+        <p><strong>§ 3 Rechte als Forum-Nutzer/Forum-Nutzerinnen</strong></p>
+        <p>(1) Selbst erstellte Kommentare, Beiträge sowie der Account können jederzeit von eingeloggten Nutzern gelöscht oder bearbeitet werden. Für die Löschung gilt dabei folgendes: Wird ein Nutzerkonto gekündigt, so werden automatisch alle erstellten Daten (Beiträge und Kommentare) gelöscht. Wird ein Beitrag gelöscht, so werden außerdem alle zugehörigen Kommentare gelöscht. Von Nutzern erstellte Kommentare können ohne Eigeneinwirkung gelöscht werden, falls der Besitzer des entsprechenden Beitrags diesen löscht.</p>
+        <p>(2) Der Nutzer kann über <a href="drittAnbieter.php"> Opt-In </a> entscheiden ob  Drittanbieter Anwendungen eingebunden und genutzt werden sollen. Diese können im Nachhinein über den selben Weg wieder wiederrufen werden.</p>
+        <p>&nbsp;</p>
+        <p><strong>§ 4 Übertragung von Nutzungsrechten</strong></p>
+        <p>Sie, als Forum-Nutzer/Forum-Nutzerin&nbsp; haben die alleinige Verantwortung des Urheberrechts i.S.d. Urhebergesetzes bei Veröffentlichung von Beiträgen und Themen im Forum zu beachten. Als Nutzer/Nutzerin räumen Sie lediglich uns mit Veröffentlichung Ihres Beitrages auf deren Homepage das Recht ein, den Beitrag dauerhaft zum Abruf bereitzustellen. Ferner hat unser Team das Recht, Ihre Themen und Beiträge zu löschen, zu bearbeiten und innerhalb seiner Homepage zu verschieben, um diese mit anderen Inhalten zu verknüpfen oder zu schließen.</p>
+        <p>&nbsp;</p>
+        <p><strong>§ 5 Haftungsbeschränkung</strong></p>
+        <p>(1) Wir übernimmen keinerlei Gewähr für die im Forum veröffentlichten und eingestellten Beiträge, Themen, externen Links und die daraus resultierenden Inhalte, insbesondere nicht für deren Richtigkeit, Vollständigkeit und Aktualität. Wir sind auch nicht verpflichtet, permanent die übermittelten und gespeicherten Beiträge der Nutzer/Nutzerinnen zu überwachen oder nach den Umständen zu erforschen, ob sie auf einen rechtswidrigen Inhalt hinweisen. Wir haften grundsätzlich nur im Falle einer vorsätzlichen oder grob fahrlässigen Pflichtverletzung.</p>
+        <p>(2) WIr weisen ausdrücklich darauf hin, dass die juristischen Beiträge und Diskussionen im Forum vollkommen unverbindlich sind. Die Nutzung der Beiträge und deren Verwertung erfolgt auf Ihre eigene Gefahr.</p>
+        <p>(3)&nbsp;&nbsp; Bei Werbeschaltungen übernimmen wir keine Haftung für den Inhalt und die Richtigkeit. Für den Inhalt der Werbeanzeigen ist der jeweilige Autor einzig und allein verantwortlich; gleiches gilt für den Inhalt der beworbenen Webseite. Bei Darstellung der Werbeanzeige auf unserer Webseite , sind wir nicht gleichzeitig mit dem rechtswidrigen Inhalt einverstanden. Daher liegt die Haftung ausschließlich bei dem Werbekunden.</p>
+        <p>(4)&nbsp;&nbsp; Es ist wird nicht für einen ständigen unterbrechungsfreien Abruf der Webseite garantiert. Einer Haftung diesbezüglich wird hiermit ausdrücklich widersprochen. Auch bei großer Sorgfalt können Ausfallzeiten leider nicht ausgeschlossen werden.</p>
+        <p>&nbsp;</p>
+        <p><strong>§ 6 Urheberrecht</strong></p>
+        <p>Sämtliche Texte, Bilder und andere auf unserer Webseite veröffentlichten Informationen und Daten unterliegen - sofern nicht anders gekennzeichnet - dem Copyright unserer Seite. Jede Form von Wiedergabe und/oder Modifikation darf nur mit der schriftlichen Genehmigung durch uns erfolgen. Andersfalls behalten wir uns das Recht vor gerichtlich gegen diese Rechtsverletzung vorzugehen. Alle Kosten, die durch eine Rechtsverletzung seitens eines Benutzers verursacht werden, werden diesem in Rechnung gestellt.</p>
+        <p><strong>&nbsp;</strong></p>
+        <p><strong>§ 7 Änderungsvorbehalt</strong></p>
+        <p>Wir haben jederzeit das Recht die Nutzungsbedingungen zu Ändern. Die Änderung wird dann per Forum-Eintrag auf der Webseite veröffentlicht.</p>
+        <p>&nbsp;</p>
+        <p><strong>§ 8 Kündigung und Laufzeit der Mitgliedschaft bei unserer Webseite <br /></strong></p>
+        <p>(1) Die Laufzeit der Mitgliedschaft beginnt mit der Registrierung und mit dem Einverständnis unseren Nutzungsbedingungen, sowie der Bestätigung der Email und besteht auf eine unbestimmte Zeit. Die Mitgliedschaft kann jederzeit ohne Angabe von Gründen fristlos gekündigt werden. </p>
+        <p>(2) Der Account kann jederzeit von eingeloggten Nutzern gelöscht werden. Für die Löschung gilt dabei folgendes: Wir ein Nutzerkonto gekündigt, so werden automatisch alle erstellten Daten (Beiträge und Kommentare) gelöscht. Wird ein Beitrag gelöscht, so werden außerdem alle zugehörigen Kommentare gelöscht. Von Nutzern erstellte Kommentare können ohne Eigeneinwirkung gelöscht werden, falls der Besitzer des entsprechenden Beitrags diesen löscht. </p>
+        <p>&nbsp;</p>
+        <p><strong>§ 9 Salvatorische Klausel</strong></p>
+        <p>Diese Forum-Nutzungsbedingung ist als Teil unserer Webseite zu betrachten, von dem aus auf diese Seite verwiesen wird. Sind einzelne Formulierungen dieser Forum-Nutzungsbedingung nicht mehr ganz oder nicht mehr vollständig konform mit der geltenden Rechtslage, so ist davon auszugehen, dass die übrigen Regelungen der Forum-Nutzungsbedingungen bestehen bleiben.</p>
         
-        <h2>Allgemeine Hinweise für die Nutzung dieses Online-Angebots</h2>
+        <p>Diese <a href="http://www.jurarat.de/muster-nutzungsbedingungen">Nutzungsbedingungen</a> wurden freundlicherweise von www.jurarat.de zur Verfügung 
 
-        <p>
-            Sie müssen mindestens 16 Jahre alt sein, um sich auf dieser Plattform zu registrieren. <br>
-            Verwenden Sie in Ihren Meldungen und Beiträgen keine Namen von fremden, realen Personen und laden Sie keine Fotos hoch, die andere Personen abbilden. Sie verletzen sonst deren Persönlichkeitsrechte. <br>
-            Alle von den Teilnehmenden eingestellten Inhalte stehen unter der CC BY-SA-Lizenz, das heißt, sie dürfen unter Nennung des (Nutzer-)Namens des Autors sowie der URL der jeweiligen Seite auf dieser Plattform unentgeltlich und unter den gleichen Bedingungen weiter verwendet werden. <br>
-
-            Im Übrigen gelten für die Nutzung dieser Internetseite die Gesetze der Bundesrepublik Deutschland. <br>
-
-            Bitte beachten Sie auch unsere Hinweise zum Datenschutz. <br>
-        </p>
-        <h2>Datenlöschung</h2>
-        <p>
-            Selbst erstellte Kommentare, Beiträge sowie der Account können jederzeit von eingeloggten Nutzern gelöscht oder bearbeitet werden. Für die Löschung gilt dabei folgendes:<br>
-            Wird ein Nutzerkonto gekündigt, so werden automatisch alle erstellten Daten (Beiträge und Kommentare) gelöscht. Wird ein Beitrag gelöscht, so werden außerdem alle zugehörigen Kommentare gelöscht. Von Nutzern erstellte Kommentare können ohne Eigeneinwirkung
-            gelöscht werden, falls der Besitzer des entsprechenden Beitrags diesen löscht.
-        </p>
     </main>
 
     <?php include_once "php/footer.php" ?>
