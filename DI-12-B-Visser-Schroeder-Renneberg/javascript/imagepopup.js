4f2a0386e8f3b49e9dae9982b263ece9f422ee29
Robin <robe123@hotmail.de>
2023-07-30 16:50:23 +0200

    image popup

--- DI-12-B-Visser-Schroeder-Renneberg/javascript/imagepopup.js
+++ DI-12-B-Visser-Schroeder-Renneberg/javascript/imagepopup.js
@@ -1,6 +1,13 @@
-if (document.getElementById('img') != null) {
-    document.getElementById('img').onclick = function() {
-        var element = document.getElementById("img");
-        element.classList.toggle("image");
-    }
+var modal = document.getElementById("myModal");
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  console.log("fgef");
+  modalImg.src = this.src;
+}
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() { 
+  modal.style.display = "none";
 }
