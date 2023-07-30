bb2460a08f8214afd7d622c7760a5d2277c1c888
Robin <robe123@hotmail.de>
2023-07-30 18:37:04 +0200

    .

--- DI-12-B-Visser-Schroeder-Renneberg/php/beitrag/beitrag.php
+++ DI-12-B-Visser-Schroeder-Renneberg/php/beitrag/beitrag.php
@@ -14,7 +14,7 @@ include_once $path . "/php/head.php";
     <?php
     if ($accept_map) {
     ?>
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="">
         <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
     <?php
     }
